207d8af7e413cbb13d395f88e4e6aa4b58c05192
shimu paul <shimupaul96@gmail.com>
2025-05-19 11:27:35 +0600

    added file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
 app.use(express.json())
 
@@ -33,6 +33,77 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
+
+        const database = client.db("newapssdb");
+        const newAppsCollection = database.collection("newapps");
+
+
+
+        app.get('/newapps', async (req, res) => {
+            // console.log('data in the server', req.body)
+            const cursor = newAppsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
+
+        app.get('/newapps/:id', async (req, res) => {
+            // console.log('data in the server', req.body)
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+            const result = await newAppsCollection.findOne(query);
+            res.send(result)
+
+        })
+
+        app.post('/newapps', async (req, res) => {
+            console.log('data in the server', req.body)
+            const newUser = req.body
+            const result = await newAppsCollection.insertOne(newUser);
+            res.send(result)
+
+        })
+
+
+        app.put('/newapps/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+            const User = req.body
+
+
+            const updatedDoc = {
+                $set: {
+                    email: User.email,
+                    password: User.password
+
+
+                }
+
+
+            }
+
+            const options = { upsert: true }
+            const result = await newAppsCollection.updateOne(query, updatedDoc, options);
+            res.send(result)
+
+            // console.log(User)
+
+        })
+
+
+
+        app.delete('/newapps/:id', async (req, res) => {
+            // console.log('data in the server', req.body)
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+            const result = await newAppsCollection.deleteOne(query);
+            res.send(result)
+
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -50,3 +121,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`new app server ${port}`)
 })
+
